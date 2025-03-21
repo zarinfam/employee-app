a9ef10d361be73af5c9bbfe3a25b164fe2dae873
Saeed Zarinfam <zarinfam.s@gmail.com>
2025-03-21 17:47:24 +0100

    Fix installation script for kind on ARM

--- scripts/devtools_provision.sh
+++ scripts/devtools_provision.sh
@@ -19,8 +19,11 @@ echo "source ~/.aliases" >> ~/.zshrc
 git config --global user.name "Saeed Zarinfam"
 git config --global user.email zarinfam.s@gmail.com
 
-# Install kind For Intel Macs
+# For AMD64 / x86_64
 [ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.25.0/kind-linux-amd64
+# For ARM64
+[ $(uname -m) = aarch64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.25.0/kind-linux-arm64
+
 chmod +x ./kind
 sudo mv ./kind /usr/local/bin/kind
 

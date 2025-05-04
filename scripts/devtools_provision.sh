81ddf02088b4b839ee63e1f8de6e4320d6605fb0
Saeed Zarinfam <zarinfam.s@gmail.com>
2025-05-04 09:14:15 +0000

    Improve the dev container file

--- scripts/devtools_provision.sh
+++ scripts/devtools_provision.sh
@@ -6,8 +6,6 @@ sudo apt update
 
 sudo apt install -y nano curl zip unzip git zsh bat httpie
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 # Change the oh my zsh default theme.
 sed -i 's/ZSH_THEME=[^ ]*/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc
@@ -15,17 +13,6 @@ echo "POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true" >> ~/.zshrc
 
 cp  scripts/.aliases ~/.aliases
 echo "source ~/.aliases" >> ~/.zshrc
-
-git config --global user.name "Saeed Zarinfam"
-git config --global user.email zarinfam.s@gmail.com
-
-# For AMD64 / x86_64
-[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.25.0/kind-linux-amd64
-# For ARM64
-[ $(uname -m) = aarch64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.25.0/kind-linux-arm64
-
-chmod +x ./kind
-sudo mv ./kind /usr/local/bin/kind
 
 kind delete cluster --name employee 
 kind create cluster --name employee
